ea248860f89455eca01df1fcebc66c1303c2a18f
Rick Clark <rkingsleyclark@gmail.com>
2020-08-03 12:55:14 +0100

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const slsw = require('serverless-webpack');
+
+module.exports = {
+  target: 'node',
+  entry: slsw.lib.entries,
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  node: false,
+  optimization: {
+    minimize: false,
+  },
+  devtool: 'inline-cheap-module-source-map',
+  externals: {
+    canvas: 'commonjs canvas',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  { targets: { node: '12' }, useBuiltIns: 'usage', corejs: 3 },
+                ],
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
